808d4682179ab6fe0c48bd48043145da7977c8fe
Chris <rodcee13@gmail.com>
2023-11-13 23:01:57 -0500

    creating invoice. stopped at 69%.  continue with update manana

---
+++ app/lib/actions.ts
@@ -0,0 +1,52 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+      .number()
+      .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string(),
+  });
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+
+    // Test it out:
+    console.log(customerId);
+    console.log(typeof amount);
+};
+
+
+// //Tip: If you're working with forms that have many fields, you may want to consider using the entries()
+// method with JavaScript's Object.fromEntries()
+
+// . For example:
+
+// const rawFormData = Object.fromEntries(formData.entries())
